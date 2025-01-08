b6365cd27e6d57e535d728e6a4f1735e16ceb416
Alessandro Pireno <139802728+apireno@users.noreply.github.com>
2025-01-08 13:33:38 +0100

    Delete database.py

--- database.py
+++
@@ -1,8 +0,0 @@
-
-class Database():
-    def __init__(self,url,username,password,namespace,database):
-        self.username = username
-        self.password = password
-        self.namespace = namespace
-        self.database = database
-        self.url = url
